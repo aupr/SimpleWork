eaa65a04ab80a86f57f98c91e34315b2d2e6b3c0
Aman Ullah <amanullah.en@gmail.com>
2017-03-29 01:15:43 +0600

    fixed

--- config.php
+++ config.php
@@ -33,7 +33,7 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'simplework');
+define('DB_DATABASE', '');
 define('DB_PORT', '3306');
 define('DB_PREFIX', '');
 
